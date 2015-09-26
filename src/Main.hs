254ddc195db3805b5c887defea234847f3fc2a3b
Wieland Hoffmann <themineo@gmail.com>
2015-09-26 23:45:45 +0200

    It works!

--- src/Main.hs
+++ src/Main.hs
@@ -20,7 +20,8 @@ import qualified Network.MPD           as MPD
 import           Network.Wreq          (defaults, header, manager, postWith)
 import           Safe                  (headMay)
 import           System.Clock          (Clock (Monotonic), TimeSpec,
-                                        diffTimeSpec, getTime)
+                                        diffTimeSpec, getTime,
+                                        timeSpecAsNanoSecs)
 import           System.Environment    (lookupEnv)
 import           System.Exit           (die)
 import           System.Posix.Time     (epochTime)
@@ -69,11 +70,16 @@ instance ToJSON Request where
 data ListenBuild = Either String Listen
 
 data State = State
-  { playerStatus   :: MPD.Status
-  , song           :: Maybe MPD.Song
-  , lastClock      :: TimeSpec
-  , alreadyElapsed :: Maybe TimeSpec
+  { playerStatus             :: MPD.Status
+  , song                     :: Maybe MPD.Song
+  , lastClock                :: TimeSpec
+  , timeElapsedInCurrentSong :: Maybe TimeSpec
   }
+
+type Nanoseconds = Integer
+
+secondsToNanoseconds :: MPD.Seconds -> Nanoseconds
+secondsToNanoseconds = (*10^9)
 
 idleMPD :: IO (MPD.Response [Subsystem])
 idleMPD = withMPD (idle [PlayerS])
@@ -86,38 +92,39 @@ getCurrentSong = withMPD MPD.currentSong
 
 isListenWorthy :: Maybe State -> MPD.Status -> Bool
 isListenWorthy oldState newStatus =
-  if stateKeptPlaying&& not sameSongLaterPosition then
+  if stateKeptPlaying then
     case oldStatus of
-      Nothing -> let pl = played_long_enough newElapsed newLength in newState == MPD.Playing && isJust pl && fromJust pl
-      Just _ -> let pl = played_long_enough oldElapsed oldLength -- Check if we played the old song long enough to make it scrobbleworthy
-                    rep = playing_repeatedly oldElapsed newElapsed newLength
-                    val = (||) <$> pl <*> rep
-                in isJust val && fromJust val
+      -- If we don't know the previous state, we're most likely at the
+      -- startup phase, so just assume the song is not listen worthy
+      Nothing -> False
+      Just _ -> let pl = played_long_enough (fromJust oldElapsed) (fromJust oldLength) -- Check if we played the old song long enough to make it scrobbleworthy
+                    -- rep = playing_repeatedly oldElapsed newElapsed newLength
+                in pl
   else False
   where newState = MPD.stState newStatus
         oldStatus = playerStatus <$> oldState
         oldPlayerState = MPD.stState <$> oldStatus
         stateKeptPlaying = (newState == MPD.Playing) && (oldPlayerState == Just MPD.Playing)
-        played_long_enough :: Maybe Double -> Maybe MPD.Seconds -> Maybe Bool
-        played_long_enough elapsed length = do
-          el <- elapsed
-          l <- length
-          return (el > 240 || l >= 30 && el > fromIntegral l / 2)
+        played_long_enough :: TimeSpec -> MPD.Seconds -> Bool
+        played_long_enough elapsed length =
+          let el = fromIntegral (timeSpecAsNanoSecs elapsed)
+              l = secondsToNanoseconds length
+              in (el > minPlayTime || l >= minLength && (fromIntegral el) > fromIntegral l / 2)
+        minPlayTime :: Nanoseconds
+        minPlayTime = secondsToNanoseconds 240
+        minLength :: Nanoseconds
+        minLength = secondsToNanoseconds 30
         -- Check if the song is playing on repeat. This doesn't work
         -- yet because prev_elapsed is from the beginning of the
         -- previous song and thus too small.
         -- TODO set up time measurement like mpdscribble does
         playing_repeatedly :: Maybe Double -> Maybe Double -> Maybe MPD.Seconds -> Maybe Bool
-        playing_repeatedly prev_elapsed elapsed length = do
-          pr_el <- prev_elapsed
-          el <- elapsed
-          l <- length
-          pl <- played_long_enough ((-) <$> prev_elapsed <*> elapsed) length
-          return (el < 60 && pr_el > el && pl)
-        newElapsed :: Maybe Double
-        newElapsed = case MPD.stTime newStatus of
-          Just (elapsed, _) -> Just elapsed
-          _ -> Nothing
+        playing_repeatedly prev_elapsed elapsed length = Just False
+          -- pr_el <- prev_elapsed
+          -- el <- elapsed
+          -- l <- length
+          -- pl <- played_long_enough ((-) <$> prev_elapsed <*> elapsed) length
+          -- return (el < 60 && pr_el > el && pl)
         newLength :: Maybe MPD.Seconds
         newLength = case MPD.stTime newStatus of
           Just (_, length) -> Just length
@@ -126,17 +133,8 @@ isListenWorthy oldState newStatus =
         oldLength = case oldStatus >>= MPD.stTime of
           Just (_, length) -> Just length
           _ -> Nothing
-        oldElapsed :: Maybe Double
-        oldElapsed = case oldStatus >>= MPD.stTime of
-          Just (elapsed, _) -> Just elapsed
-          _ -> Nothing
-        newSongID = MPD.stSongID newStatus
-        oldSongID = oldStatus >>= MPD.stSongID
-        sameSong :: Bool
-        sameSong = let v = (==) <$> newSongID <*> oldSongID in isJust v && fromJust v
-        laterPosition :: Bool
-        laterPosition = let v = (>=) <$> newElapsed <*> oldElapsed in isJust v && fromJust v
-        sameSongLaterPosition = sameSong && laterPosition
+        oldElapsed :: Maybe TimeSpec
+        oldElapsed = oldState >>= timeElapsedInCurrentSong
 
 extractElapsed :: MPD.Status -> Maybe Double
 extractElapsed s = case MPD.stTime s of
@@ -200,14 +198,14 @@ scrobble previousState newStatus = do
                Right s -> s
                _ -> Nothing
       lastClockTick = lastClock <$> previousState
-      newState = MPD.stState newStatus
+      newPlayerState = MPD.stState newStatus
       previousPlayerState = MPD.stState <$> playerStatus <$> previousState
-      currentlyElapsed = previousState >>= alreadyElapsed
-      newElapsed = calculateElapsed previousPlayerState newState lastClockTick currentClockTick currentlyElapsed
+      currentlyElapsed = previousState >>= timeElapsedInCurrentSong
+      newElapsed = calculateElapsed previousPlayerState newPlayerState lastClockTick currentClockTick currentlyElapsed
+      newState = previousState >>= (\s -> Just (s {timeElapsedInCurrentSong = newElapsed}))
   -- Perform the scrobbling, if necessary
-  -- TODO: Pass in newElapsed to compare it with the song length if the state change is playing -> playing
-  if isJust previousState && isListenWorthy previousState newStatus
-    then doScrobble (previousState >>= song)
+  if isJust previousState && isListenWorthy newState newStatus
+    then doScrobble (newState >>= song)
     else print "No scrobbling necessary"
 
   -- Wait for the next event of the player subsystem with an updated state
@@ -227,7 +225,7 @@ scrobble previousState newStatus = do
         -- the song changed and the new elapsed value should be the
         -- sum of the last elapsed value and the time between the last clock tick and the current one
         calculateElapsed (Just MPD.Playing) MPD.Playing (Just lastClockTick) currentClockTick (Just currentlyElapsed) = Just (currentlyElapsed + diffTimeSpec currentClockTick lastClockTick)
-        -- 4. If any other state change means the stopped state is
+        -- 4. Any other state change means the stopped state is
         -- involved or the song changed, so set the value to 0 as well
         calculateElapsed _ _ currentClockTick _ _ = diffTimeSpec <$> currentClockTick <*> currentClockTick
 
